e5004686172117967c3215f7598b136b737bd5f8
刘帅 <shuai.liu@beebank.com>
2019-12-30 17:19:19 +0800

    feat: 引入require.js

---
+++ node_admin/view/js/lib/jquery.simple.js
@@ -0,0 +1,11 @@
+/**
+ * 自己手写的jq主要是练习原生js的封装
+ * 命名为jquery.simple
+ * 作者: 刘晓北
+ * 邮箱: 15732451723@163.com
+ */
+(function (window){
+  var $ = '1223'
+  window.$ = $;
+  return $;
+})(window)
--- node_admin/view/js/main.js
+++ node_admin/view/js/main.js
@@ -1,8 +1,20 @@
-window.onload = function(){
-  const btn = document.querySelector('#btn');
-  btn.addEventListener('click', function(e) {
-    e = e || event
-    console.log(e.target)
-    alert('欢迎，欢迎')
-  })
-}
+require.config({
+  // 路径默认与main.js在同一个目录
+  // 基目录
+  baseUrl: "js/lib",
+  // 基目录的相对路径
+  paths: {
+  　"jquery": "jquery.simple",
+  },
+  shim: {
+    "jquery.simple": {
+      // 依赖的库
+      // deps: ['underscore', 'jquery'],
+      // 暴露的名字
+      exports: '$'
+    }
+  }
+});
+require(['jquery.simple'], function ($) {
+console.log($)
+})
