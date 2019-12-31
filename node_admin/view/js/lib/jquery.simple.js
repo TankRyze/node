506c9e6455c511c14c19e1b3ec99f206342bba05
刘帅 <shuai.liu@beebank.com>
2019-12-31 18:11:47 +0800

    chore: 试着解读jq源码并抄写常用的功能

--- node_admin/view/js/lib/jquery.simple.js
+++ node_admin/view/js/lib/jquery.simple.js
@@ -12,12 +12,40 @@
    * @param {选择器} selector 
    * @param {上下文} context 
    */
+  var 
+  // 定义当前版本
+  // jq中是节省查找内存地址时间，提高效率
+  // 这里置为了装逼
+  core_version = '0.0.1',
   // jq的构造函数
-  var jQuery = function (selector, context) {
-    // 返回一个jquery的fn的初始化的方法init
+  jQuery = function (selector, context) {
+    // 没有使用new 运算符将jQuery 显示的实例化，而是直接调用其函数
+    //  jQuery.fn是jQuery的原型
+    // jQuery.fn.init.prototype = jQuery.fn
+    // 所以通过这种方法生成的实例 this 所指向的 仍然是是jQuery 的原型
     return new jQuery.fn.init(selector, context)
   }
-  jQuery.fn  = jQuery.prototype = {};
+  jQuery.fn  = jQuery.prototype = {
+    // 当前版本
+    jquery: core_version,
+    // 构造函数
+    constructor: jQuery,
+    // 初始化方法
+    // 我们是通过new jQuery.fn.init()构造的jQuery对象，所有这个方法称作：jQuery对象构造器，
+    init: function (selector, context, rootjQuery) {
+
+      // 例如： $(''), $(null), $(undefined), $(false)
+      // 如果传入参数为空直接返回this
+      // 处理，'', null, undefined, fasle 返回this，增加程序的健壮性
+      if (!selector) return this;
+      
+      // 选择器传入的是字符串
+      // 处理字符串
+      if (typeof selector === 'string') {
+
+      }
+    }
+  };
   // 核心方法
 	// 回调系统
 	// 异步队列
